e85f0267f40ad5f488a4c34a37f06c763b478763
Monu Alam <monualam828@gmail.com>
2025-02-26 20:54:07 +0530

    documentation of application is created by using openapi

--- src/main/java/com/curd/config/OpenApiConfig.java
+++ src/main/java/com/curd/config/OpenApiConfig.java
@@ -6,14 +6,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.tags.Tag;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
 
 @Configuration
 public class OpenApiConfig {
 
-	
+//	http://localhost:8082/swagger-ui/index.ht
 	@Bean
 	public OpenAPI openAPI() {
 	
@@ -26,7 +31,21 @@ public class OpenApiConfig {
 			    	).servers(List.of(
 							new Server().url("http://localhost:8082").description("This is local server"),
 							new Server().url("http://localhost:8080/").description("This is Live Server"))
-							);
+							)
+				.tags(List.of(
+						new Tag().name("Login API"))
+			
+					) .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+            .components(new Components().addSecuritySchemes(
+                    "bearerAuth",
+                    new SecurityScheme()
+                            .type(SecurityScheme.Type.HTTP)  
+                            .scheme("bearer")
+                            .bearerFormat("JWT")
+                            .in(SecurityScheme.In.HEADER)
+                            .name("Authorization")
+            ));
+				
 		
 	}
 }
